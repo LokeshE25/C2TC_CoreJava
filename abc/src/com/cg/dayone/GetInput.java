6a760376e8ed63d4fc180e48f36a9d978fc9a2a9
Lenovo <Lenovo@Lenovo>
2023-07-28 15:40:16 +0530

    Day 1

---
+++ abc/src/com/cg/dayone/GetInput.java
@@ -0,0 +1,17 @@
+package com.cg.dayone;
+import java.util.Scanner;
+
+public class GetInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the string:");
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		System.out.println(a);
+		System.out.println("Enter the number:");
+		int b=sc.nextInt();
+		System.out.println(b);
+	}
+
+}
